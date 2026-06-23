bc113765fb69c239d5e04367434831a8eb7007ff
AhnafTaiyeb310 <ahnaftaiyeb1@gmail.com>
2026-06-23 20:19:42 +0600

    feat: implement DRF ModelViewSet and router for /api/tasks CRUD

--- backend/apps/tasks/views.py
+++ backend/apps/tasks/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from . import models
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+class TaskModelViewSet(ModelViewSet):
+    queryset = models.Task.objects.all().order_by('-created_at')
+    serializer_class = TaskSerializer
